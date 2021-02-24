da3d58f7d56ce0ec627f3a815d2018c68f0e2187
shobhitm23 <smakhij@purdue.edu>
2021-02-24 15:07:19 -0500

    server with routes

---
+++ api/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+
+const app = express(),
+      bodyParser = require("body-parser")
+      port = 3080
+
+
+app.use(bodyParser.json());
+
+
+// Routes
+
+app.get('/', (req, res) => {
+  res.json('Main page')
+});
+
+app.get('/api/signup', (req, res) => {
+  console.log('api/signup')
+  res.json("Signup Page")
+});
+
+app.get('/api/login', (req, res) => {
+  console.log('api/signup')
+  res.json("Signup Page")
+});
+
+app.get('/api', (req, res) => {
+  console.log('api/')
+  res.json("API Page")
+});
+
+
+
+// Listening port
+
+app.listen(port, () => {
+  console.log(`Server listening on the port::${port}`);
+});
